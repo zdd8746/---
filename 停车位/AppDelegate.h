d852f40a662e8aadf598fa3beef2148afede01c6
zdd8746 <jsjzdd_n@126.com>
2014-05-07 18:23:22 +0800

    Initial Commit

---
+++ 停车位/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  停车位
+//
+//  Created by 123 on 14-5-7.
+//  Copyright (c) 2014年 神马. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 停车位/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  停车位
+//
+//  Created by 123 on 14-5-7.
+//  Copyright (c) 2014年 神马. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
